ce896d6056ef285894442d1a089caaa504914507
poseidon <61605433+poseidonKey@users.noreply.github.com>
2023-09-29 14:44:02 +0900

    riverpod 강좌 전 마무리

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class _App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(fontFamily: 'NotoSans'),
+      theme: ThemeData(fontFamily: 'NotoSans '),
       home: const SplashScreen(),
     );
   }

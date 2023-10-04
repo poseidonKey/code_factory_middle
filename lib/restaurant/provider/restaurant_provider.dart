78e88163bfb50d6fc4fa13b947f373b82591dbe3
poseidon <jsy1030@naver.com>
2023-10-04 14:17:19 +0900

    section9 restaurant pagination 1 까지

--- lib/restaurant/provider/restaurant_provider.dart
+++ lib/restaurant/provider/restaurant_provider.dart
@@ -20,7 +20,21 @@ class RestaurantStateNotifier extends StateNotifier<CursorPaginationBase> {
     paginate();
   }
 
-  void paginate() async {
+  void paginate(
+      {int fetchCount = 20,
+      //추가로 데이터 더 가져오기
+      // true : 추가로 데이터 더 가져옴
+      // false : 새로 고침(현재 상태를 덮어 씌움)
+      bool fetchMore = false,
+      // 강제로 다시 로딩하기
+      // true의 경우 : CursorPageinationLoading()
+      bool forceRefetch = false}) async {
+    // State의 5가지 가능성.. 즉 상태가
+    // 1. CursorPagination : 정상적으로 데이터가 있는 상태
+    // 2. CursorPaginationLoading : 데이터가 로딩 중인 상태(현재 캐시 없음)
+    // 3. CursorPaginationError : 에러가 있는 상태
+    // 4. CursorPaginationRefetching : 첫번째 페이지부터 다시 데이터를 가져올 때
+    // 5. CursorPaginationFetchMore : 추가 데이터를 paginate 해 오라는 요청을 받았을 때
     final resp = await repository.paginate();
     state = resp;
   }

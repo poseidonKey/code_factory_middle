b41618d047620939bbd00e4204c2d162bb77a53d
poseidon <jsy1030@naver.com>
2023-10-04 14:03:19 +0900

    약간의 오류 수정

--- lib/common/model/pagination_params.dart
+++ lib/common/model/pagination_params.dart
@@ -9,4 +9,5 @@ class PaginationParams {
   const PaginationParams({this.after, this.count});
   factory PaginationParams.fromJson(Map<String, dynamic> json) =>
       _$PaginationParamsFromJson(json);
+  Map<String, dynamic> toJson() => _$PaginationParamsToJson(this);
 }

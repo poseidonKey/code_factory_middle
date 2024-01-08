1f5f1667953a7faca1e1698dfb0ad9b2bcf836c8
poseidon <61605433+poseidonKey@users.noreply.github.com>
2024-01-08 14:36:15 +0900

    섹션9 restaurant pagination-4 까지

--- lib/common/model/pagination_params.dart
+++ lib/common/model/pagination_params.dart
@@ -7,6 +7,16 @@ class PaginationParams {
   final int? count;
 
   const PaginationParams({this.after, this.count});
+  PaginationParams copyWith({
+    String? after,
+    int? count,
+  }) {
+    return PaginationParams(
+      after: after ?? this.after,
+      count: count ?? this.count,
+    );
+  }
+
   factory PaginationParams.fromJson(Map<String, dynamic> json) =>
       _$PaginationParamsFromJson(json);
   Map<String, dynamic> toJson() => _$PaginationParamsToJson(this);

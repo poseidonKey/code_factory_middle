d50f6245958ae9801d9bd0bb7689b07072a91416
poseidon <61605433+poseidonKey@users.noreply.github.com>
2024-01-10 13:23:31 +0900

    섹션11 ratingModel 작업하기

--- lib/common/utils/data_utils.dart
+++ lib/common/utils/data_utils.dart
@@ -4,4 +4,12 @@ class DataUtils {
   static String pathToURL(String value) {
     return 'http://$ip$value';
   }
+
+  static List<String> listPathsToUrls(List<String> paths) {
+    return paths
+        .map(
+          (e) => pathToURL(e),
+        )
+        .toList();
+  }
 }

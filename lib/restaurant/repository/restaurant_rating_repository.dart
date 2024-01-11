b34dbfb5ac526ab298fa421f8c542fc393cc361e
poseidon <jsy10303@gmail.com>
2024-01-10 16:31:59 -0800

    temp

---
+++ lib/restaurant/repository/restaurant_rating_repository.dart
@@ -0,0 +1,30 @@
+import 'package:code_factory_middle/common/const/data.dart';
+import 'package:code_factory_middle/common/dio/dio.dart';
+import 'package:code_factory_middle/common/model/cursor_pagination_model.dart';
+import 'package:code_factory_middle/common/model/pagination_params.dart';
+import 'package:code_factory_middle/rating/model/rating_model.dart';
+import 'package:dio/dio.dart' hide Headers;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:retrofit/retrofit.dart';
+part 'restaurant_rating_repository.g.dart';
+
+final restaurantRatingProvider =
+    Provider.family<RestaurantRatingRepository, String>((ref, id) {
+// http://restaurant/ip/:rid/rating
+  final dio = ref.watch(dioProvider);
+  return RestaurantRatingRepository(dio,
+      baseUrl: 'http://restaurant/$ip/$id/rating');
+});
+
+@RestApi()
+abstract class RestaurantRatingRepository {
+  factory RestaurantRatingRepository(Dio dio, {String baseUrl}) =
+      _RestaurantRatingRepository;
+  @GET('/')
+  @Headers({
+    'accessToken': 'true',
+  })
+  Future<CursorPagination<RatingModel>> paginate({
+    @Queries() PaginationParams? paginationParams = const PaginationParams(),
+  });
+}

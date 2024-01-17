15aac8e9bdb088933ceb7447bdb7d0b0b848e86e
poseidon <jsy10303@gmail.com>
2024-01-17 13:26:44 +0900

    섹션 17 - OrderRepository 작업하기작업하기

---
+++ lib/order/repository/order_repository.dart
@@ -0,0 +1,44 @@
+import 'package:code_factory_middle/common/const/data.dart';
+import 'package:code_factory_middle/common/dio/dio.dart';
+import 'package:code_factory_middle/common/model/cursor_pagination_model.dart';
+import 'package:code_factory_middle/common/model/pagination_params.dart';
+import 'package:code_factory_middle/common/repository/base_pagination_repository.dart';
+import 'package:code_factory_middle/order/model/order_model.dart';
+import 'package:code_factory_middle/order/model/post_order_body.dart';
+import 'package:dio/dio.dart' hide Headers;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:retrofit/http.dart';
+
+part 'order_repository.g.dart';
+
+final orderRepositoryProvider = Provider<OrderRepository>(
+  (ref) {
+    final dio = ref.watch(dioProvider);
+
+    return OrderRepository(dio, baseUrl: 'http://$ip/order');
+  },
+);
+
+// http://$ip/order
+@RestApi()
+abstract class OrderRepository
+    implements IBasePaginationRepository<OrderModel> {
+  factory OrderRepository(Dio dio, {String baseUrl}) = _OrderRepository;
+
+  @override
+  @GET('/')
+  @Headers({
+    'accessToken': 'true',
+  })
+  Future<CursorPagination<OrderModel>> paginate({
+    @Queries() PaginationParams? paginationParams = const PaginationParams(),
+  });
+
+  @POST('/')
+  @Headers({
+    'accessToken': 'true',
+  })
+  Future<OrderModel> postOrder({
+    @Body() required PostOrderBody body,
+  });
+}
